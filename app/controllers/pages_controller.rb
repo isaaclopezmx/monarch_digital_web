5955680645cff34112d011f74cdeb12fb8357db6
Isaac Lopez <isaac.t.lopez@gmail.com>
2023-08-07 14:21:40 -0600

    created backend code for website

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def home; end
+  def about; end
+  def services; end
+  def our_work; end
+  def contact; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'pages#home', as: 'home'
+  get 'about', to: 'pages#about', as: 'about'
+  get 'services', to: 'pages#services', as: 'services'
+  get 'our-work', to: 'pages#our_work', as: 'our_work'
+  get 'contact', to: 'pages#contact', as: 'contact'
   # Defines the root path route ("/")
   # root "articles#index"
 end
