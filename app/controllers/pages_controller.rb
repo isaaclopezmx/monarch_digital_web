f68d785724b030599037faf9732ee7ec438e43f1
Isaac Lopez <isaac.t.lopez@gmail.com>
2023-08-11 19:51:21 -0600

    hero section upload

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def home; end
   def about; end
   def services; end
-  def our_work; end
+  def portfolio; end
   def contact; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home', as: 'home'
   get 'about', to: 'pages#about', as: 'about'
   get 'services', to: 'pages#services', as: 'services'
-  get 'our-work', to: 'pages#our_work', as: 'our_work'
+  get 'portfolio', to: 'pages#portfolio', as: 'portfolio'
   get 'contact', to: 'pages#contact', as: 'contact'
   # Defines the root path route ("/")
   # root "articles#index"
